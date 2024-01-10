26d9abf207cf03e51a52e54c838f3c7c3a7c6e59
ewi josepha <ewijosepha@gmail.com>
2024-01-10 16:27:26 +0100

    indenting and removing death code from app

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -24,18 +24,19 @@ form.addEventListener('submit', (event) => {
 
     .then((data) => {
       if (data.message) {
-        console.log('user not found')
         output.innerHTML = '<P>Profile is not Valid</P>'
       } else {
+
         // viewing github profile
+
         output.innerHTML = `<div class="picture"><img src="${data.avatar_url}"><h4>Profile-Name: ${data.login}</p><a href = '${data.html_url}' target='_blank'>View-Profile</a> <p>Bio:${data.bio}</p>
         </div>
         <div class="flexingfeatures"> 
         <strong>Following:${data.following}</strong>,  <strong>Followers:${data.followers}</strong> <strong>Repositories:${data.public_repos}</strong>
         </div>`
       }
-      console.log(data)
     })
+    
     .catch((error) => {
       console.log(error)
     })
